14b73120ade0fb6853c21ff884812205483a74de
Peter Chang <JyChang012@outlook.com>
2021-04-11 12:08:52 +0800

    manually merge with zongyue

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from utils.misc import make_env, get_args, set_seed
 from runner import Runner
 
-
 if __name__ == '__main__':
     set_seed(78)
     args = get_args()
     env = make_env(args)
     runner = Runner(args, env)
     if args.evaluate:
-        ave_return, norm_return = runner.evaluate(render=False)
+        ave_return, norm_return = runner.evaluate(render=True)
     else:
         runner.run()
